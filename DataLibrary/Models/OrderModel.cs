d06b5e9daa94f454b52c0f7fc98343b007cce2a5
Ben Matthies <ben_jamie_fett@msn.com>
2021-06-25 12:43:03 -0600

    Add project files.

---
+++ DataLibrary/Models/OrderModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "Keep to 20 characters.")]
+        [MinLength(3, ErrorMessage = "Must be at least 3 characters.")]
+        [DisplayName("Name for the Order")]
+        public string OrderName { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        [DisplayName("Meal")]
+        [Range(1, int.MaxValue, ErrorMessage = "Select a meal from the list")]
+        public int FoodId { get; set; }
+        [Required]
+        [Range(1, 10, ErrorMessage = "Must be between one and ten.")]
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+    }
+}
